initial training... it is quite terrible - Inf on test, 1.000 (returns 0) on train. Normalising the x might help... but it looks like somthing else is probably wrong.

egnn/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn import Linear, Module
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
@@ -9,22 +10,24 @@ from torch_geometric.nn import global_mean_pool
 class EGNN(Module):
     def __init__(self):
         super(EGNN, self).__init__()
-        self.conv1 = GCNConv(4, 64) # 4 because of the 4 node features 
-        self.conv2 = GCNConv(64, 32)
-        self.conv3 = GCNConv(32, 16)
+        self.conv1 = GCNConv(4, 16) # 4 because of the 4 node features 
+        self.conv2 = GCNConv(16, 16) # TODO: check if edge features could be used... for distance
+        self.conv3 = GCNConv(16, 16)
         self.lin = Linear(16, 1)
 
     def forward(self, x, edge_index, batch):
         x = self.conv1(x, edge_index)
-        x = x.relu()
+        # x = x.sigmoid()
         x = self.conv2(x, edge_index)
-        x = x.relu()
+        # x = x.sigmoid()
         x = self.conv3(x, edge_index)
-        
+        # x = x.sigmoid()
+
         x = global_mean_pool(x, batch) 
 
-        # x = F.dropout(x, p=0.5, training=self.training)
+        x = F.dropout(x, p=0.5, training=self.training)
         x = self.lin(x)
-        x = x.relu() # added
-
+        # print(x)
+        x = x.sigmoid() # added
+        # print(x)
         return x

train.py
@@ -17,11 +17,53 @@ torch.manual_seed(42)
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
 
-
 # https://colab.research.google.com/drive/1I8a0DfQ3fI7Njc62__mVXUlcAleUclnb?usp=sharing
 
 model = EGNN()
-print(model)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+criterion = torch.nn.MSELoss()
 
-for step, data in enumerate(train_loader):
-    print(f"Step {step}: {data.num_graphs} graphs")
+def train():
+    model.train()
+    for data in train_loader:  # Iterate in batches over the training dataset.
+        cR = data.colR.view(data.colR.size(0), -1)
+        cG = data.colR.view(data.colG.size(0), -1)
+        cB = data.colR.view(data.colB.size(0), -1)
+        a = data.atom.view(data.atom.size(0), -1)
+        x = torch.hstack((cR,cG,cB,a)).to(torch.float32)
+        out = model(x, data.edge_index, data.batch)  # Perform a single forward pass.
+        loss = criterion(out, data.dE_scaled.to(torch.float32).view(len(data.dE_scaled), -1))  # Compute the loss.
+        loss.backward()  # Derive gradients.
+        optimizer.step()  # Update parameters based on gradients.
+        optimizer.zero_grad()  # Clear gradients.
+
+def test(loader):
+     model.eval()
+     sum=0
+     apes = None
+     for data in loader:  # Iterate in batches over the training/test dataset.
+         cR = data.colR.view(data.colR.size(0), -1)
+         cG = data.colR.view(data.colG.size(0), -1)
+         cB = data.colR.view(data.colB.size(0), -1)
+         a = data.atom.view(data.atom.size(0), -1)
+         x = torch.hstack((cR,cG,cB,a)).to(torch.float32)
+         out = model(x, data.edge_index, data.batch).detach()
+         #print("out")
+         #print(out)
+         real = data.dE_scaled.to(torch.float32).view(len(data.dE_scaled), -1).detach()
+         #print("real")
+         #print(real)
+         ape = (real-out).abs()/real.abs()
+         if apes is None: apes = ape
+         else: apes = torch.vstack((apes, ape))
+         #break
+     # print(apes)
+     # print(apes.sum(dim=0))
+     return apes.nanmean()
+
+for epoch in range(1, 1000):
+    train()
+    train_acc = test(train_loader)
+    test_acc = test(test_loader)
+    print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+
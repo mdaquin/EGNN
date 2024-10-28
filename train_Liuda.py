32daaf00596b68cbb95dd87e7077f9bfaf0ac1ed
Liudmyla Klochko <klochko.lyuda@gmail.com>
2024-10-28 11:03:34 +0100

    gpu calcs

--- train_Liuda.py
+++ train_Liuda.py
@@ -11,14 +11,16 @@ import copy, time
 
 def train():
     model.train()
-    for data in train_loader: 
-        # Iterate in batches over the training dataset.
+    for data in train_loader:  # Iterate in batches over the training dataset.
         data   = data.to(device).cuda()
         cR = data.colR.view(data.colR.size(0), -1)
         cG = data.colR.view(data.colG.size(0), -1)
         cB = data.colR.view(data.colB.size(0), -1)
         a = data.atom.view(data.atom.size(0), -1)
-        x = torch.hstack((cR,cG,cB,a)).to(torch.float32)
+        f = data.fluoride.view(data.fluoride.size(0), -1)
+        m = data.metal.view(data.metal.size(0), -1)
+        k = data.potassium.view(data.potassium.size(0), -1)
+        x = torch.hstack((cR,cG,cB,a,m,f,k)).to(torch.float32)
         x = x.to(device).cuda()
         out = model(x, data.edge_index, data.batch, data.distance.to(torch.float).view(len(1-data.distance), -1)).cuda()  # Perform a single forward pass.
         out[out == float("Inf")] = 0    
@@ -29,46 +31,47 @@ def train():
         optimizer.step()  # Update parameters based on gradients.
         optimizer.zero_grad()  # Clear gradients.
 
-def test(model, loader, show=False):
+def test(model, loader, show=False, clear=False):
      model.eval()
      sum=0
      errs = None
      if show: toshow = None
      for data in loader:  # Iterate in batches over the training/test dataset.
-         data   = data.cuda()
+         data   = data.to(device).cuda()
          cR = data.colR.view(data.colR.size(0), -1)
          cG = data.colR.view(data.colG.size(0), -1)
          cB = data.colR.view(data.colB.size(0), -1)
          a = data.atom.view(data.atom.size(0), -1)
-         x = torch.hstack((cR,cG,cB,a)).to(torch.float32)
+         f = data.fluoride.view(data.fluoride.size(0), -1)
+         m = data.metal.view(data.metal.size(0), -1)
+         k = data.potassium.view(data.potassium.size(0), -1)
+         x = torch.hstack((cR,cG,cB,a,m,f,k)).to(torch.float32)
          x=x.cuda()
          out = model(x, data.edge_index, data.batch, data.distance.to(torch.float).view(len(1-data.distance), -1)).detach()
-         
          real = data.dE_scaled.to(torch.float32).view(len(data.dE_scaled), -1).detach()
-         
          err = (real-out).abs()
-         
          if errs is None: errs = err
          else: errs = torch.vstack((errs, err))
          if show:
              if toshow is None: toshow = torch.hstack((real,out)).cpu()
-             else: toshow = torch.vstack(torch.hstack(real, out)).cpu()
+             else: toshow = torch.vstack((toshow.to(device), torch.hstack((real, out)))).cpu()
          #break
      # print(apes)
      # print(apes.sum(dim=0))
      if show: 
          # print(toshow)
-         plt.clf()
-         plt.scatter(toshow.T[0], toshow.T[1])
+         if clear: plt.clf()
+         c="b" if not clear else "lightgrey"
+         plt.scatter(toshow.T[0], toshow.T[1], color=c)
          plt.plot([0.0, 1.0], [0.0, 1.0], color="r")
-         plt.draw()
-         plt.pause(0.0001)
+         if not clear: plt.draw()
+         if not clear: plt.pause(0.0001)
      return errs.nanmean()
 
 plt.ion()
 plt.show()
 torch.cuda.manual_seed(42) 
-device = "cuda" 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 print("RUNNIN ON", device)
 
 train_dataset = torch.load("data/train.pt", weights_only=False)
@@ -81,15 +84,13 @@ print(f'First graph:{train_dataset[0]}')
 print(f'Number of test graphs: {len(test_dataset)}')
 print(f'First graph:{test_dataset[0]}')
 
-# TODO: normalise distances... make no difference currently
-
 # TODO: batch size as option
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=200, shuffle=False)
-model = EGNN(hidden_channels=64, K=1).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001) # LR in params
-# criterion = torch.nn.MSELoss() # TODO: in params
-criterion = torch.nn.L1Loss() 
+model = EGNN(hidden_channels=128, K=2).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00005) # LR in params
+criterion = torch.nn.MSELoss() # TODO: in params
+# criterion = torch.nn.L1Loss() 
 
 best_test = None
 best_epoch = None
@@ -102,7 +103,7 @@ for epoch in range(1, nepoch+1):
     tt = round((time.time()-t1)*1000)
     ttt += tt
     t1 = time.time()
-    train_acc = test(model, train_loader)
+    train_acc = test(model, train_loader, show=True, clear=True)
     test_acc = test(model, test_loader, show=True)
     te = round((time.time()-t1)*1000)
     tte += te
@@ -110,7 +111,7 @@ for epoch in range(1, nepoch+1):
         best_test = test_acc
         best_model = copy.deepcopy(model)
         best_epoch = epoch
-    print(f'Epoch: {epoch:03d} ({tt:04d}/{te:04d}), Train MAE: {train_acc:.4f}, Test MAE: {test_acc:.4f}')
+    print(f'Epoch: {epoch:03d} ({tt:04d}/{te:04d}), Train MAE: {train_acc:.4f}, Test MAE: {test_acc:.4f} (best: {best_test:.4f})')
 
 print("Best MAE on test", best_test,"at",best_epoch)
 print(f"Total time {round(ttt/1000):04d}s for training, {round(tte/1000):04d}s for testing")

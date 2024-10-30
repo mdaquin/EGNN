c721bcdebc58b3b0f5581dd64f26475d34e685f0
Liudmyla Klochko <klochko.lyuda@gmail.com>
2024-10-30 14:51:26 +0100

    create_graph_changes

--- create_graph_dataset_L.py
+++ create_graph_dataset_L.py
@@ -31,27 +31,7 @@ def distMM(nbm1, nbm2, a, b, c):
     met2 = posM(nbm2)
     return dist(met1[0], met1[1], met1[2], met2[0], met2[1], met2[2], a, b, c)
 
-def graph_from_line(l, G=None, colors=[]):
-  ng = l.name
-  if G is None: G=nx.Graph(dE_scaled=l["dE scaled"])
-  for i in range(1, 9):
-    col = l[f"Color Metal{i}"].lower()
-    colR, colG, colB = 1 if col=="r" else 0, 1 if col=="v" else 0, 1 if col=="b" else 0
-    G.add_node(f"{ng}_M{i}", colR=colR, colG=colG, colB=colB, atom=l["Z"], metal=1, fluoride=0, potassium=0) #, dE_scaled=l["dE scaled"])
-    colors.append(col if col != "v" else "g")
-  for i in range(9,21):
-    G.add_node(f"{ng}_F{i}", colR=0, colG=0, colB=0, atom=9, metal=0, fluoride=1, potassium=0) #, dE_scaled=l["dE scaled"])
-    colors.append("lightgrey")
-  G.add_node(f"{ng}_K", colR=0, colG=0, colB=0, atom=19, metal=0, fluoride=0, potassium=1) #, dE_scaled=l["dE scaled"])
-  colors.append("lightgrey")
 
-  # adding direct links between every metal and every other metal
-  # TODO: should there be a link between every F atom as well? and F and the other metals?
-  for i in range(1,8):
-     for j in range(i+1,9):
-        G.add_edge(f"{ng}_M{i}", f"{ng}_M{j}", distance=np.round(distMM(i,j,l["a"],l["b"],l["c"]), 3))
-
-  return G, colors
 
 def displayGraph(G, ng, colors):
     pos = {f"{ng}_M1": (-0.5, -0.5), f"{ng}_M2": (-0.5, 1), f"{ng}_M3": (1, -0.5), f"{ng}_M4": (1, 1),
@@ -80,11 +60,11 @@ def graph_from_line_vec(l, G=None, colors=[]):
     colors.append(col if col != "v" else "g")
   for i in range(9,21):
 
-    node_feature = [0, 0, 0, 9, 0, 1, 0]  # colR, colG, colB, atom, metal, fluoride, potassium
-    G.add_node(f"{ng}_F{i}", node_feature=node_feature) #, dE_scaled=l["dE scaled"])
+    node_feature = [0, 0, 0, 9, 0, 1, 0] 
+    G.add_node(f"{ng}_F{i}", node_feature=node_feature) 
     colors.append("lightgrey")
-  node_feature = [0, 0, 0, 19, 0, 0, 1]  # colR, colG, colB, atom, metal, fluoride, potassium
-  G.add_node(f"{ng}_K", node_feature=node_feature) #, dE_scaled=l["dE scaled"])
+  node_feature = [0, 0, 0, 19, 0, 0, 1]  
+  G.add_node(f"{ng}_K", node_feature=node_feature)
   colors.append("lightgrey")
 
   # adding direct links between every metal and every other metal
@@ -93,7 +73,7 @@ def graph_from_line_vec(l, G=None, colors=[]):
      for j in range(i,9):
         G.add_edge(f"{ng}_M{i}", f"{ng}_M{j}", distance=np.round(distMM(i,j,l["a"],l["b"],l["c"]), 3))
 
-  return G, colors
+  return G, colors,ng
 
 # add on hot encoding for PK and M
 # add direct relations between Ms
@@ -101,11 +81,6 @@ def graph_from_line_vec(l, G=None, colors=[]):
 # search for neighborhood level in GNN
 
 
-def displayLargeGraph(G, colors):
-    plt.figure(figsize=(10,10))
-    # should create positions myself...
-    nx.draw(G, with_labels=True, node_size=1000, node_color=colors)
-    plt.show()
 
 if __name__ == "__main__":
     print("*"*6,"loading Data", "*"*6)
@@ -114,9 +89,18 @@ if __name__ == "__main__":
     # normalise output
     # TODO: test standardisation ? 
     df["dE scaled"] = ((df["dE scaled"] - df["dE scaled"].min()) / (df["dE scaled"].max()-df["dE scaled"].min()))
+    
+    linenb= 0
+    G,colors,ng = graph_from_line_vec(df.iloc[linenb])
+    displayGraph(G, ng, colors)
+    
+    
     train_df = df.sample(int(len(df)*0.8), random_state=42)
     test_df = df.drop(train_df.index)
     train_list = []
+    
+    
+    
     for l in train_df.iloc: train_list.append(from_networkx(graph_from_line_vec(l)[0]))
     # normalise... 
     test_list = []
@@ -126,6 +110,7 @@ if __name__ == "__main__":
     test = EGNNDataset(test_list)
     torch.save(train, "train_vec.pt")
     torch.save(test, "test_vec.pt")
+    
 
     
 
